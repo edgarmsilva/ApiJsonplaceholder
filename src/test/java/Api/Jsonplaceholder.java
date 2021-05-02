15a758e68dcfdd8f3b35951951a8dc8aa21210f0
edgarmoreirasilva <edgar.silva@e-deploy.com.br>
2021-05-02 15:36:18 -0300

    finished the main scenarios

--- src/test/java/Api/Jsonplaceholder.java
+++ src/test/java/Api/Jsonplaceholder.java
@@ -12,34 +12,13 @@ import static stepDefinitions.Hooks.*;
 
 public class Jsonplaceholder {
 
-//    public static int GetUserId(String username) {
-//        RequestSpecification httpRequest = RestAssured.given().spec(api_jsonplaceholder);
-//        response = httpRequest.get("/users?username=" + username);
-//        UsersResponse[] userBody = response.getBody().as(UsersResponse[].class);
-//        Assert.assertEquals(username, userBody[0].username);
-//        return userBody[0].id;
-//    }
-
     public static UsersResponse[] GetUser(String username) {
         RequestSpecification httpRequest = RestAssured.given().spec(api_jsonplaceholder);
         response = httpRequest.get("/users?username=" + username);
         return response.getBody().as(UsersResponse[].class);
     }
 
-//    public static ArrayList GetUserPostsIds(Integer id) {
-//        ArrayList<Integer> postIds = new ArrayList<Integer>();
-//        RequestSpecification httpRequest = RestAssured.given().spec(api_jsonplaceholder);
-//        response = httpRequest.get("/posts?userId=" + id);
-//        PostsResponse[] postsBody = response.getBody().as(PostsResponse[].class);
-//        for (PostsResponse eachPost : postsBody) {
-//            Assert.assertEquals(id, eachPost.userId);
-//            postIds.add(eachPost.id);
-//        }
-//        return postIds;
-//    }
-
     public static PostsResponse[] GetUserPostsIds(Integer id) {
-        ArrayList<Integer> postIds = new ArrayList<>();
         RequestSpecification httpRequest = RestAssured.given().spec(api_jsonplaceholder);
         response = httpRequest.get("/posts?userId=" + id);
         return response.getBody().as(PostsResponse[].class);
@@ -53,13 +32,29 @@ public class Jsonplaceholder {
             PostComments[] postsComments = response.getBody().as(PostComments[].class);
             for (PostComments eachComment : postsComments) {
                 Assert.assertEquals(postId, eachComment.postId);
-                if (validateEmail(eachComment.email)){
+                if (validateEmail(eachComment.email)) {
                     commentEmails.add(eachComment.email);
-                }else{
-                    System.out.println("The Email: "+ eachComment.email + "From comment ID No."+ eachComment.id +" is not a valid Email.");
+                } else {
+                    System.out.println("The Email: " + eachComment.email + "From comment ID No." + eachComment.id + " is not a valid Email.");
                 }
             }
         }
         return commentEmails;
+    }
+
+    public static PostComments[] GetUserPostsComments(ArrayList<Integer> postId) {
+        RequestSpecification httpRequest = RestAssured.given().spec(api_jsonplaceholder);
+        response = httpRequest.get("/comments?postId=" + postId);
+        httpRequest.log().all();
+        PostComments[] postsComments = response.getBody().as(PostComments[].class);
+        return response.getBody().as(PostComments[].class);
+    }
+
+    public static PostComments[] GetUserPostComment(Integer postId) {
+        RequestSpecification httpRequest = RestAssured.given().spec(api_jsonplaceholder);
+        response = httpRequest.get("/comments?postId=" + postId);
+        httpRequest.log().all();
+        PostComments[] postsComments = response.getBody().as(PostComments[].class);
+        return response.getBody().as(PostComments[].class);
     }
 }
--- src/test/java/stepDefinitions/ApiJsonPlaceholder_steps.java
+++ src/test/java/stepDefinitions/ApiJsonPlaceholder_steps.java
@@ -12,9 +12,10 @@ public class ApiJsonPlaceholder_steps {
     Integer userNameId;
     UsersResponse[] userResponse;
     PostsResponse[] postsResponses;
+    Object[] response;
     ArrayList<Integer> postIds = new ArrayList<>();
 
-    @Given("that I search for the username {string} to get its id")
+    @Given("that I search for the username {string}")
     public void that_i_search_for_the_username(String username) {
         userResponse = GetUser(username);
         Assert.assertEquals(username, userResponse[0].username);
@@ -33,6 +34,32 @@ public class ApiJsonPlaceholder_steps {
 
     @Then("I validate if the emails in the comment section are in the proper format")
     public void i_validate_if_the_emails_in_the_comment_section_are_in_the_proper_format() {
-        GetUserPostsCommentsEmails(postIds);
+        GetUserPostsComments(postIds);
     }
+
+    @When("I search for the username {string}")
+    public void i_search_for_the_username(String username) {
+        userResponse = GetUser(username);
+    }
+
+    @When("I search for a inexistent username {string}")
+    public void i_search_for_a_inexistent_username(String username) {
+        response = (UsersResponse[]) GetUser(username);
+    }
+
+    @Then("I receive an empty body response")
+    public void i_receive_an_empty_body() {
+        Assert.assertEquals(0, response.length);
+    }
+
+    @When("I use an inexistent user id to search for the posts")
+    public void i_use_an_inexistent_user_id_to_search_for_the_posts() {
+        response = (PostsResponse[]) GetUserPostsIds(9999);
+    }
+
+    @When("I use an inexistent post id to search for the posts comments")
+    public void i_use_an_inexistent_post_id_to_search_for_the_posts_comments() {
+        response = (PostComments[]) GetUserPostComment(9999);
+    }
+
 }
