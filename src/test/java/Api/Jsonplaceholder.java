88849b3939cf4baec2dd82c0b4e8b93130c790b3
edgarmoreirasilva <edgar.silva@e-deploy.com.br>
2021-05-02 10:59:39 -0300

    configuring circleCI

--- src/test/java/Api/Jsonplaceholder.java
+++ src/test/java/Api/Jsonplaceholder.java
@@ -12,42 +12,49 @@ import static stepDefinitions.Hooks.*;
 
 public class Jsonplaceholder {
 
-    public static int GetUserId(String username) {
+//    public static int GetUserId(String username) {
+//        RequestSpecification httpRequest = RestAssured.given().spec(api_jsonplaceholder);
+//        response = httpRequest.get("/users?username=" + username);
+//        UsersResponse[] userBody = response.getBody().as(UsersResponse[].class);
+//        Assert.assertEquals(username, userBody[0].username);
+//        return userBody[0].id;
+//    }
+
+    public static UsersResponse[] GetUser(String username) {
         RequestSpecification httpRequest = RestAssured.given().spec(api_jsonplaceholder);
-//        httpRequest.log().all();
         response = httpRequest.get("/users?username=" + username);
-        UsersResponse[] userBody = response.getBody().as(UsersResponse[].class);
-        Assert.assertEquals(username, userBody[0].username);
-        return userBody[0].id;
+        return response.getBody().as(UsersResponse[].class);
     }
 
-    public static ArrayList GetUserPostsIds(Integer id) {
-        ArrayList<Integer> postIds = new ArrayList<Integer>();
+//    public static ArrayList GetUserPostsIds(Integer id) {
+//        ArrayList<Integer> postIds = new ArrayList<Integer>();
+//        RequestSpecification httpRequest = RestAssured.given().spec(api_jsonplaceholder);
+//        response = httpRequest.get("/posts?userId=" + id);
+//        PostsResponse[] postsBody = response.getBody().as(PostsResponse[].class);
+//        for (PostsResponse eachPost : postsBody) {
+//            Assert.assertEquals(id, eachPost.userId);
+//            postIds.add(eachPost.id);
+//        }
+//        return postIds;
+//    }
+
+    public static PostsResponse[] GetUserPostsIds(Integer id) {
+        ArrayList<Integer> postIds = new ArrayList<>();
         RequestSpecification httpRequest = RestAssured.given().spec(api_jsonplaceholder);
-//        httpRequest.log().all();
         response = httpRequest.get("/posts?userId=" + id);
-        PostsResponse[] postsBody = response.getBody().as(PostsResponse[].class);
-        for (PostsResponse eachPost : postsBody) {
-            Assert.assertEquals(id, eachPost.userId);
-            postIds.add(eachPost.id);
-        }
-        return postIds;
+        return response.getBody().as(PostsResponse[].class);
     }
 
     public static ArrayList<String> GetUserPostsCommentsEmails(ArrayList<Integer> postIds) {
-        ArrayList<String> commentEmails = new ArrayList<String>();
+        ArrayList<String> commentEmails = new ArrayList<>();
         for (Integer postId : postIds) {
-//            System.out.println("ID:" + postId);
             RequestSpecification httpRequest = RestAssured.given().spec(api_jsonplaceholder);
-//            httpRequest.log().all();
             response = httpRequest.get("/comments?postId=" + postId);
             PostComments[] postsComments = response.getBody().as(PostComments[].class);
             for (PostComments eachComment : postsComments) {
-//                System.out.println("Validating Email:"+ eachComment.email);
                 Assert.assertEquals(postId, eachComment.postId);
                 if (validateEmail(eachComment.email)){
                     commentEmails.add(eachComment.email);
-//                    System.out.println("Email OK!");
                 }else{
                     System.out.println("The Email: "+ eachComment.email + "From comment ID No."+ eachComment.id +" is not a valid Email.");
                 }
@@ -55,5 +62,4 @@ public class Jsonplaceholder {
         }
         return commentEmails;
     }
-
 }
--- src/test/java/stepDefinitions/ApiJsonPlaceholder_steps.java
+++ src/test/java/stepDefinitions/ApiJsonPlaceholder_steps.java
@@ -1,24 +1,34 @@
 package stepDefinitions;
 
-import Api.Jsonplaceholder;
+import ResponseModels.*;
 import io.cucumber.java.en.*;
+import org.junit.Assert;
 
 import java.util.ArrayList;
 
 import static Api.Jsonplaceholder.*;
 
 public class ApiJsonPlaceholder_steps {
-    int userId;
-    ArrayList<Integer> postIds = new ArrayList<Integer>();
+    Integer userNameId;
+    UsersResponse[] userResponse;
+    PostsResponse[] postsResponses;
+    ArrayList<Integer> postIds = new ArrayList<>();
 
     @Given("that I search for the username {string} to get its id")
     public void that_i_search_for_the_username(String username) {
-        userId = GetUserId(username);
+        userResponse = GetUser(username);
+        Assert.assertEquals(username, userResponse[0].username);
+        userNameId = userResponse[0].id;
     }
 
     @When("I use the id fetched to search for the posts written by the user")
     public void i_use_the_id_fetched_to_search_for_the_posts_written_by_the_user() {
-        postIds = GetUserPostsIds(userId);
+        postsResponses = GetUserPostsIds(userNameId);
+        ArrayList<Integer> postIds = new ArrayList<>();
+        for (PostsResponse eachPost : postsResponses) {
+            Assert.assertEquals(userNameId, eachPost.userId);
+            postIds.add(eachPost.id);
+        }
     }
 
     @Then("I validate if the emails in the comment section are in the proper format")
